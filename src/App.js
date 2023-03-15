54d93694f9f00fba0f679267e1d47a0c6cffdef4
Gil Sala <gilsala_88@hotmail.com>
2023-03-15 10:12:49 +0100

    Add wellcome screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import "./App.css";
 import Escena from "./components/Escena/Escena";
 import frases from "./frases";
+import { useState } from "react";
 
 function App() {
+  const Principal = () => {
+    const [isPrincipal, setPrincipal] = useState(true);
+    const buttonHandler = () => {
+      setPrincipal(false);
+    };
+
+    return (
+      <>
+        <div>
+          {isPrincipal ? (
+            <div className="box-welcome">
+              <div className="welcome">
+                <h3>Welcome !</h3>
+                <p>This is my first app with React. I am learning and using:</p>
+                <ul>
+                  <li>Rendering lists</li>
+                  <li>UseState hook</li>
+                  <li>Condicitional rending</li>
+                </ul>
+                <p className="last-text">
+                  After you click the button, you can interact with the back and
+                  next button <br /> to navigate through the story
+                </p>
+                <button onClick={buttonHandler}>Click to start</button>
+              </div>
+            </div>
+          ) : (
+            <Escena frases={frases} />
+          )}
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
-      <Escena frases={frases} />
+      <Principal />
     </>
   );
 }
