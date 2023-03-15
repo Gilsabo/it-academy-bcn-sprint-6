2607b71e6b3ba0da1a79a7e87690aa4b7ea25cb4
Gil Sala <gilsala_88@hotmail.com>
2023-03-15 10:12:09 +0100

    Translate sentences from spanish to english

--- src/frases.js
+++ src/frases.js
@@ -1,8 +1,28 @@
-  const frases = ["El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-  "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-  "L'heroi va decidir travessar la porta que el portava a casa",
-  "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
-]
+import imageOne from "./img/1.jpg";
+import imageTwo from "./img/2.jpg";
+import imageThee from "./img/3.jpg";
+import imageFour from "./img/4.jpg";
 
+const frases = [
+  {
+    txt: "Our hero was floating in outer space when he spotted a spaceship in the distance.",
+    img: imageOne,
+  },
 
-export default frases
+  {
+    txt: "He was curious about the interior of the ship and started to inspect it. He arrived at a room with two doors.",
+    img: imageTwo,
+  },
+
+  {
+    txt: "The hero decided to go through the door that led him home.",
+    img: imageThee,
+  },
+
+  {
+    txt: "Meanwhile, other heroes were not so lucky in their choice..",
+    img: imageFour,
+  },
+];
+
+export default frases;
