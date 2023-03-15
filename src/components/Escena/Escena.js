c8ae0f95a6d289e77ebb2e7b7a818affaf9b633a
Gil Sala <gilsala_88@hotmail.com>
2023-03-15 10:13:43 +0100

    Solve sprint-6

--- src/components/Escena/Escena.js
+++ src/components/Escena/Escena.js
@@ -1,34 +1,33 @@
 import React from "react";
 import StyledEscena from "./StyledEscena";
 import { useState } from "react";
-import StylesScreenButtons from "../ScreenButtons/StyledScreenButtons";
+import ScreenButtons from "../ScreenButtons/ScreenButtons";
+
 
 const Escena = ({ frases }) => {
-  const [indexFrase, selectedIndex] = useState(0);
+  const [indexFrase, setIndexFrase] = useState(0);
 
   const paintAnterior = () => {
-    if(indexFrase === 0){
-    return console.log(0)
+    if (indexFrase === 0) {
+      return;
     }
-    selectedIndex(indexFrase - 1);
+    setIndexFrase(indexFrase - 1);
+
   };
   const paintSeguent = () => {
-    if(indexFrase === 3){
-        return console.log(1)
-        
+    if (indexFrase === 3) {
+      return;
     }
-    selectedIndex(indexFrase + 1);
+    setIndexFrase(indexFrase + 1);
   };
 
   return (
     <div>
       <div>
-        <StylesScreenButtons onClick={paintAnterior}>
-          Anterior
-        </StylesScreenButtons>
-        <StylesScreenButtons onClick={paintSeguent}>
-          Següent
-        </StylesScreenButtons>
+        <ScreenButtons
+          paintAnterior={paintAnterior}
+          paintSeguent={paintSeguent}
+        />
       </div>
       <div>
         {frases.map((fraseItem, index) => (
@@ -38,56 +37,14 @@ const Escena = ({ frases }) => {
               backgroundColor: indexFrase === index ? "#FFC0CB" : "white",
             }}
           >
-            {fraseItem}
+            {fraseItem.txt}
           </StyledEscena>
         ))}
       </div>
+      <div className="background" style={{ backgroundImage: `url(${frases[indexFrase].img})` }}></div>
     </div>
   );
 };
 
 export default Escena;
 
-/*{ frases.map((frase)=>(
-    <StyledEscena className="escena" >{frase}</StyledEscena>
-    ))}*/
-
-// #ffc0cb
-
-/*if(secondRef.current.style.backgroundColor === color ){
-            setColor('#FFC0CB')
-            secondRef.current.style.backgroundColor=  'white'
-            firstRef.current.style.backgroundColor= color
-           console.log('1a')
-        }else if(thirdRef.current.style.backgroundColor === '#FFC0CB' ){
-            setColor('#FFC0CB')
-            
-            thirdRef.current.style.backgroundColor=   'white'
-            
-            secondRef.current.style.backgroundColor= color
-            console.log('2a')
-        }else if(fourthRef.current.style.backgroundColor !== '' && fourthRef.current.style.backgroundColor ==='white' ){
-            setColor('#FFC0CB')
-            fourthRef.current.style.backgroundColor=  'white'
-            thirdRef.current.style.backgroundColor=  color
-
-            console.log('3a')
-        } */
-
-/* if((secondRef.current.style.backgroundColor === '' || secondRef.current.style.backgroundColor === 'white')  && firstRef.current.style.backgroundColor!==''){
-        setColor('rgb(255, 192, 203)')
-        secondRef.current.style.backgroundColor=  color
-       firstRef.current.style.backgroundColor= 'white'
-       console.log(1)
-    }else if((thirdRef.current.style.backgroundColor=== '' || thirdRef.current.style.backgroundColor=== 'white') && secondRef.current.style.backgroundColor!=='' ){
-        setColor('#FFC0CB')
-        thirdRef.current.style.backgroundColor=  color
-       secondRef.current.style.backgroundColor= 'white'
-       console.log(2)
-    }else if((fourthRef.current.style.backgroundColor=== '' || fourthRef.current.style.backgroundColor=== 'white') && thirdRef.current.style.backgroundColor!=='' ){
-        setColor('#FFC0CB')
-        fourthRef.current.style.backgroundColor=  color
-        thirdRef.current.style.backgroundColor= 'white'
-    } */
-
-/*{{backgroundColor: color2 === 'white' ? 'white' : color1}} */
