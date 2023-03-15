c19737baf189af08c5221fde731e03b3eb0fd70c
Gil Sala <gilsala_88@hotmail.com>
2023-03-15 10:14:41 +0100

    Solve sprint-6

--- src/components/Escena/StyledEscena.js
+++ src/components/Escena/StyledEscena.js
@@ -1,12 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const StyledEscena = styled.div`
-border: 1px solid black;
-border-radius: 10px;
-padding-top:10px;
-padding-bottom:10px;
-margin-top:10px;
-martin-bottom:10px;
-text-align:center;
-`
-export default StyledEscena
+  border: 1px solid black;
+  border-radius: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-top: 10px;
+  martin-bottom: 10px;
+  text-align: center;
+`;
+export default StyledEscena;
